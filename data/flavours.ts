aa249edc04c3694a06bd0c0330c832019c53e0be
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-03-23 18:50:31 +0700

    Add the falvours

---
+++ data/flavours.ts
@@ -0,0 +1,49 @@
+import type { Flavour } from '@/types/flavour'
+
+export const flavours: Flavour[] = [
+	{
+		id: 'blackberry',
+		name: 'Blackberry Jam',
+		subtitle: 'Rich, dark, luxurious.',
+		sceneImage: '/images/scenes/blackberry-scene.png',
+		jarImage: '/images/jars/blackberry-jar.png',
+		bgFrom: '#140d1f',
+		bgTo: '#4c2b6b',
+	},
+	{
+		id: 'strawberry',
+		name: 'Strawberry Jam',
+		subtitle: 'Bright, ripe, elegant.',
+		sceneImage: '/images/scenes/strawberry-scene.png',
+		jarImage: '/images/jars/strawberry-jar.png',
+		bgFrom: '#2a0d14',
+		bgTo: '#9b2d43',
+	},
+	{
+		id: 'raspberry',
+		name: 'Raspberry Jam',
+		subtitle: 'Bright, juicy, refined.',
+		sceneImage: '/images/scenes/raspberry-scene.png',
+		jarImage: '/images/jars/raspberry-jar.png',
+		bgFrom: '#2a0d1d',
+		bgTo: '#b03b7a',
+	},
+	{
+		id: 'orange',
+		name: 'Orange Marmalade',
+		subtitle: 'Citrus, glossy, warm.',
+		sceneImage: '/images/scenes/orange-scene.png',
+		jarImage: '/images/jars/orange-jar.png',
+		bgFrom: '#2b160a',
+		bgTo: '#c96b1d',
+	},
+	{
+		id: 'kiwi',
+		name: 'Kiwi Jam',
+		subtitle: 'Fresh, crisp, vibrant.',
+		sceneImage: '/images/scenes/kiwi-scene.png',
+		jarImage: '/images/jars/kiwi-jar.png',
+		bgFrom: '#0f1d10',
+		bgTo: '#5a8f3b',
+	},
+]
