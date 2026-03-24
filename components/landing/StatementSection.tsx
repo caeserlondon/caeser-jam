9031d11fe9826ca6e248d2464bb7782e6ff91e91
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-03-24 22:55:40 +0700

    Update the statement section

--- components/landing/StatementSection.tsx
+++ components/landing/StatementSection.tsx
@@ -1,34 +1,43 @@
 export default function StatementSection() {
 	return (
-		<section className='relative overflow-hidden bg-[#a9c986] pt-10'>
-			<div className='relative mt-10 rounded-t-[50%_14%] bg-[#ede7da] pt-24 pb-20'>
-				<div className='mx-auto max-w-[1800px] px-4'>
-					<svg
-						viewBox='0 0 1800 520'
-						className='h-auto w-full'
-						aria-label='Design and build by Caeser Ibrahim'
-					>
-						<path
-							id='statement-arc'
-							d='M 80 380 Q 900 80 1720 380'
-							fill='transparent'
-						/>
-						<text
-							fill='#111111'
-							fontSize='118'
-							fontWeight='300'
-							letterSpacing='2'
+		<section className='relative bg-[#ede7da] pb-20 pt-12 overflow-hidden'>
+			{/* top cream arc */}
+			<div className='absolute inset-x-0 top-0 -translate-y-full leading-none'>
+				<svg
+					viewBox='0 0 1440 180'
+					preserveAspectRatio='none'
+					className='block h-[180px] w-full'
+					aria-hidden='true'
+				>
+					<path
+						d='M0,180 C260,70 1180,70 1440,180 L1440,180 L0,180 Z'
+						fill='#ede7da'
+					/>
+				</svg>
+			</div>
+
+			<div className='mx-auto max-w-[1800px] px-4'>
+				<svg
+					viewBox='0 0 2400 650'
+					className='h-auto w-full overflow-visible'
+					aria-label='Design and build by Caeser Ibrahim'
+				>
+					<path
+						id='statement-arc'
+						d='M 120 500 Q 1200 120 2280 500'
+						fill='transparent'
+					/>
+
+					<text fill='#111111' fontSize='92' fontWeight='300' letterSpacing='1'>
+						<textPath
+							href='#statement-arc'
+							startOffset='50%'
+							textAnchor='middle'
 						>
-							<textPath
-								href='#statement-arc'
-								startOffset='50%'
-								textAnchor='middle'
-							>
-								DESIGN &amp; BUILD BY CAESER IBRAHIM
-							</textPath>
-						</text>
-					</svg>
-				</div>
+							DESIGN &amp; BUILD BY CAESER IBRAHIM
+						</textPath>
+					</text>
+				</svg>
 			</div>
 		</section>
 	)
