e291c49826c68604cf5f8e7493097d5d6fa1c9d5
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-03-26 19:17:44 +0700

    Create the icons

---
+++ components/products/ProductIcons.tsx
@@ -0,0 +1,69 @@
+export function ArrowLeftIcon() {
+	return (
+		<svg
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			strokeWidth='1.8'
+			className='h-5 w-5'
+		>
+			<path d='M15 18l-6-6 6-6' />
+		</svg>
+	)
+}
+
+export function ArrowRightIcon() {
+	return (
+		<svg
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			strokeWidth='1.8'
+			className='h-5 w-5'
+		>
+			<path d='M9 18l6-6-6-6' />
+		</svg>
+	)
+}
+
+export function PlusIcon() {
+	return (
+		<svg
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			strokeWidth='1.8'
+			className='h-5 w-5'
+		>
+			<path d='M12 5v14M5 12h14' />
+		</svg>
+	)
+}
+
+export function MinusIcon() {
+	return (
+		<svg
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			strokeWidth='1.8'
+			className='h-5 w-5'
+		>
+			<path d='M5 12h14' />
+		</svg>
+	)
+}
+
+export function CheckIcon() {
+	return (
+		<svg
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			strokeWidth='1.8'
+			className='h-4 w-4'
+		>
+			<path d='M6 12l4 4 8-8' />
+		</svg>
+	)
+}
