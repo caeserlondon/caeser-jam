8b5b8d7acde9d74c8fbacaadb13bae014d7d5723
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-03-28 17:50:25 +0700

    Update the config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
 	images: {
 		formats: ['image/avif', 'image/webp'],
 		imageSizes: [32, 48, 64, 96, 128, 192, 256, 320, 384, 448, 640],
+		qualities: [60, 65, 75],
 	},
 	reactCompiler: true,
 }
